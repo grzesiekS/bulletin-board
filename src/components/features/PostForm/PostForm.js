71716e0fca520e6d4a34e23d19459ed1d214052f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-11 16:41:57 +0200

    Add form to PostFomr component

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -3,35 +3,64 @@ import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getSelectedPost } from '../../../redux/postsRedux';
+import { getAllStatus } from '../../../redux/statusRedux';
 
 import styles from './PostForm.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>PostForm</h2>
-    {children}
-  </div>
-);
+
+class Component extends React.Component {
+
+  render() {
+    const {className, selectedPost, allStatus} = this.props;
+    return (
+      <div className={clsx(className, styles.root)}>
+        <div className={styles.container}>
+          <form>
+            <label htmlFor='title'>Title:</label>
+            <input type='text' id='title' maxLength='10' value={selectedPost.title}></input>
+            <label htmlFor='description'>Description:</label>
+            <input type='text' id='description' maxLength='20' value={selectedPost.description}></input>
+            <label htmlFor='statusId'>Status:</label>
+            <select name='statusId' id='statusId' defaultValue={selectedPost.statusId}>
+              {allStatus.map(status => (
+                <option key={status.id} value={status.id}>{status.statusName}</option>
+              ))}
+            </select>
+            <label htmlFor='price'>Price:</label>
+            <input id='price' type='number' min='0' value={selectedPost.price}></input>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  selectedPost: PropTypes.object,
+  allStatus: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+Component.defaultProps = {
+  selectedPost: [],
+  allStatus: [],
+};
+
+const mapStateToProps = (state, props) => ({
+  selectedPost: getSelectedPost(state, props.postId),
+  allStatus: getAllStatus(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as PostForm,
-  // Container as PostForm,
+  // Component as PostForm,
+  Container as PostForm,
   Component as PostFormComponent,
 };
