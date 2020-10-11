18336b4121d85028e2532c70dcae079722b7edc9
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-11 11:52:08 +0200

    Add postInfo and userInfo sections in Post component

--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -5,24 +5,52 @@ import clsx from 'clsx';
 
 import { connect } from 'react-redux';
 import { getSelectedPost } from '../../../redux/postsRedux';
+import { getUserById } from '../../../redux/usersRedux';
 
 import styles from './Post.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Post</h2>
-    {children}
-  </div>
-);
-
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
+const Component = ({className, post, user}) => {
+  console.log(post);
+  console.log(user);
+  return(
+    <div className={clsx(className, styles.root)}>
+      <div className={styles.postInfo}>
+        <h2>Post Title: {post.title}</h2>
+        <p>{post.description}</p>
+        <p>Price: {post.price}€</p>
+        <p>Upload: {post.uploadDate}</p>
+        <p>Update: {post.updateDate}</p>
+      </div>
+      <div className={styles.userInfo}>
+        <h2>Contact Me</h2>
+        <p>Author: {user.userName}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phoneNo}</p>
+      </div>
+    </div>
+  );
 };
 
-const mapStateToProps = (state, props) => ({
-  post: getSelectedPost(state, props.match.params.id),
-});
+Component.propTypes = {
+  className: PropTypes.string,
+  post: PropTypes.object,
+  user: PropTypes.object,
+};
+
+Component.defaultProps = {
+  user: [],
+  post: [],
+};
+
+const mapStateToProps = (state, props) => {
+  const post = getSelectedPost(state, props.match.params.id);
+  const user = getUserById(state, post.userId);
+
+  return {
+    post,
+    user,
+  };
+};
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
