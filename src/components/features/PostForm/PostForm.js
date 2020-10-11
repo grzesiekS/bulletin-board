58aab5d0da3e75f347f443a29ab5bd783d510ca7
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-11 17:50:26 +0200

    Add onChange function to inputs in PostForm

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 import { connect } from 'react-redux';
-import { getSelectedPost } from '../../../redux/postsRedux';
+import { getSelectedPost, updatePost } from '../../../redux/postsRedux';
 import { getAllStatus } from '../../../redux/statusRedux';
 
 import styles from './PostForm.module.scss';
@@ -13,23 +13,45 @@ import styles from './PostForm.module.scss';
 class Component extends React.Component {
 
   render() {
-    const {className, selectedPost, allStatus} = this.props;
+    const {className, selectedPost, allStatus, updatePost} = this.props;
     return (
       <div className={clsx(className, styles.root)}>
         <div className={styles.container}>
           <form>
             <label htmlFor='title'>Title:</label>
-            <input type='text' id='title' maxLength='10' value={selectedPost.title}></input>
+            <input
+              type='text'
+              id='title'
+              minLength='10'
+              value={selectedPost.title || ''}
+              onChange={event => updatePost(event.currentTarget.value ,event.currentTarget.id, selectedPost.id)}
+            />
             <label htmlFor='description'>Description:</label>
-            <textarea id='description' maxLength='20' value={selectedPost.description}></textarea>
+            <textarea
+              id='description'
+              minLength='20'
+              value={selectedPost.description}
+              onChange={event => updatePost(event.currentTarget.value ,event.currentTarget.id, selectedPost.id)}
+            />
             <label htmlFor='statusId'>Status:</label>
-            <select name='statusId' id='statusId' defaultValue={selectedPost.statusId}>
+            <select
+              name='statusId'
+              id='statusId'
+              defaultValue={selectedPost.statusId}
+              onChange={event => updatePost(event.currentTarget.value ,event.currentTarget.id, selectedPost.id)}
+            >
               {allStatus.map(status => (
                 <option key={status.id} value={status.id}>{status.statusName}</option>
               ))}
             </select>
             <label htmlFor='price'>Price:</label>
-            <input id='price' type='number' min='0' value={selectedPost.price}></input>
+            <input
+              id='price'
+              type='number'
+              min='0'
+              value={selectedPost.price}
+              onChange={event => updatePost(event.currentTarget.value ,event.currentTarget.id, selectedPost.id)}
+            />
           </form>
         </div>
       </div>
@@ -41,6 +63,7 @@ Component.propTypes = {
   className: PropTypes.string,
   selectedPost: PropTypes.object,
   allStatus: PropTypes.array,
+  updatePost: PropTypes.func,
 };
 
 Component.defaultProps = {
@@ -53,11 +76,11 @@ const mapStateToProps = (state, props) => ({
   allStatus: getAllStatus(state),
 });
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  updatePost: (value,id, postId) => dispatch(updatePost({value,id,postId})),
+});
 
-const Container = connect(mapStateToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   // Component as PostForm,
