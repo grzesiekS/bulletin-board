a2377946df073301081d68bec2acc782d3f25441
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-21 16:06:45 +0200

    Implement fetchSelectedPost in Post component

--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -5,13 +5,19 @@ import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 
 import { connect } from 'react-redux';
-import {getLoadingStatus, getSelectedPost } from '../../../redux/postsRedux';
+import { getSelectedPost, fetchSelectedPost, getLoadingStatus } from '../../../redux/postsRedux';
 import { getUserById, getCurrentUser } from '../../../redux/usersRedux';
 
 import styles from './Post.module.scss';
 import { Button } from '../../common/Button/Button';
 
 class Component extends React.Component {
+
+  componentDidMount = () => {
+    const {selectedPost} = this.props;
+
+    selectedPost(this.props.match.params.id);
+  }
 
   statusRender = status => (
     <p>Status: <span className={clsx(styles[status], styles.thicken)}>{status}</span></p>
@@ -31,29 +37,39 @@ class Component extends React.Component {
 
   render() {
 
-    const {className, post} = this.props;
+    const {className, post, loadingStatus} = this.props;
 
     return (
       <div>
-        <div className={clsx(className, styles.root)}>
-          <div className={styles.container}>
-            <div className={styles.postInfo}>
-              <h2>Post Title: {post.title}</h2>
-              <p>{post.description}</p>
-              <p>Price: <span className={styles.thicken}>{post.price}€</span></p>
-              <p>Upload: <span className={styles.thicken}>{post.uploadDate}</span></p>
-              <p>Update: <span className={styles.thicken}>{post.updateDate}</span></p>
-              {this.statusRender(post.status.statusName)}
+
+        {loadingStatus !== undefined
+          ?
+          !loadingStatus.active && post.user !== undefined && post.status !== undefined
+            ?
+            <div className={clsx(className, styles.root)}>
+              <div className={styles.container}>
+                <div className={styles.postInfo}>
+                  <h2>Post Title: {post.title}</h2>
+                  <p>{post.description}</p>
+                  <p>Price: <span className={styles.thicken}>{post.price}€</span></p>
+                  <p>Upload: <span className={styles.thicken}>{post.uploadDate}</span></p>
+                  <p>Update: <span className={styles.thicken}>{post.updateDate}</span></p>
+                  {this.statusRender(post.status.statusName)}
+                </div>
+                <div className={styles.userInfo}>
+                  <h2>Contact Me</h2>
+                  <p>Author: <span className={styles.thicken}>{post.user.userName}</span></p>
+                  <p>Email: <span className={styles.thicken}>{post.user.email}</span></p>
+                  <p>Phone: <span className={styles.thicken}>{post.user.phoneNo}</span></p>
+                </div>
+              </div>
+              {/* {this.buttonRender(currentUserInfo, user, post)} */}
             </div>
-            <div className={styles.userInfo}>
-              <h2>Contact Me</h2>
-              <p>Author: <span className={styles.thicken}>{post.user.userName}</span></p>
-              <p>Email: <span className={styles.thicken}>{post.user.email}</span></p>
-              <p>Phone: <span className={styles.thicken}>{post.user.phoneNo}</span></p>
-            </div>
-          </div>
-          {/* {this.buttonRender(currentUserInfo, user, post)} */}
-        </div>
+            :
+            null
+          :
+          null
+        }
       </div>
     );
   }
@@ -99,7 +115,11 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-const Container = connect(mapStateToProps)(Component);
+const mapDispatchToProps = dispatch => ({
+  selectedPost: id => dispatch(fetchSelectedPost(id)),
+});
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   // Component as Post,
